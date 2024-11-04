ddabcd1aeffb240fc0689301f555dd87e329c0b1
adisri06 <adi.sri06@gmail.com>
2024-11-04 13:28:46 +0530

    first commit

---
+++ src/app/users/layout.js
@@ -0,0 +1,8 @@
+export default function UsersLayout({ children }) {
+    return (
+      <div>
+        {/* You can add any users-specific layout here */}
+        {children}
+      </div>
+    );
+  }
---
+++ src/app/users/page.js
@@ -0,0 +1,8 @@
+import React from "react";
+const Pages = ()=>{
+    return (
+        <h1>Hello from Pages</h1>
+      );
+}
+export default Pages;
+  
